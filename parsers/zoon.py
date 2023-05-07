98fa7fe667f271c54067838c964e94bb624caeab
Vladislav_Kashurkin <mymailkash@gmail.com>
2023-05-07 13:07:36 +0300

    232

--- parsers/zoon.py
+++ parsers/zoon.py
@@ -134,9 +134,11 @@ def main():
     count = 0
 
     options = webdriver.ChromeOptions()
+    options.add_argument("--no-sandbox")
     options.add_argument('--headless')
     service = webdriver.chrome.service.Service("/usr/bin/chromedriver")
     driver = webdriver.Chrome(service=service, options=options)
+
     driver = webdriver.Chrome("/usr/bin/chromedriver", options=options)
     
     try:
